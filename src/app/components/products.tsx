7dafba603190f4190db5a66e34c40d58360076a2
Sergio-Jimenez-De-Santiago <sergio.jimenez@ucdconnect.ie>
2024-11-08 11:11:11 +0000

    Fixed bugs

--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -2,37 +2,47 @@
 import { useEffect, useState } from "react";
 
 export default function Products() {
-  
-
     interface Item {
       id: number;
       name: string;
-  
-  }
+    }
     const [data, setData] = useState<any>([]);
-  
+
+    // Added empty dependency array to make sure the useEffect is only triggered on the initial load of the
+    // page instead of every time data is updated, like it did before
     useEffect(() => {
       fetch('products.json')
         .then(response => response.json())
         .then(json => setData(json));
-    });
-  
-    if (!data) {
-      return <p>Loading...</p>;
-    }
-  
+    }, []);
 
     return (
-        <div>
-      <ul>
-        {(data as Item[]).map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-      <button onClick={()=> {const newdata =  data.concat({id:"5", name: "Spaghetti"}); setData(newdata)}} value="Spaghetti">
-      Add Spaghetti
-    </button>
-    </div>      
+      <div>
+        {data.length === 0 ? ( <p>Loading...</p> ) : (
+        // Changed if condition because an empty array is not false, so !data would not work as intented.
+        // Also included the buttons when "Loading..." is on screen as I think it looks better 
+          <ul>
+            {(data as Item[]).map(item => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        )}
+
+        <button onClick={() => {
+          // Changed the id from 5 to data.length + 1 so that when adding more than 1 "Spaghetti" the ids 
+          // don't clash. Also removed value="Spaghetti" because it does not serve any purpose
+          const newdata = data.concat({id: data.length + 1, name: "Spaghetti"}); 
+          setData(newdata);
+          }}>
+        Add Spaghetti
+        </button><br></br>
+        <button onClick={() => {
+          const newdata = data.slice(0, -1); 
+          setData(newdata);
+          }}>
+        Remove last item
+        </button>
+      </div>      
     );
   }
 
